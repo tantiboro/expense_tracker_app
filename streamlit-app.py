1031b9f3447b244da24517a15f120ffa11343e79
Tanti Ouattara <tantiboro@hotmail.com>
2025-01-22 22:05:06 -0600

    first commit of the expense app

---
+++ streamlit-app.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+import os
+import smtplib
+from email.message import EmailMessage
+
+path='ExpenseTracker/ExTrackBing/'
+# path='C:/inetpub/wwwroot/image4.cf/b/'
+fileAttach= 'income.csv'
+
+# Load the variables from the .env file
+load_dotenv(r'Y:\0Python-Working\ExpenseTracker\ExTrackBing\.env')
+# load_dotenv(r'C:\inetpub\wwwroot\image4.cf\b\.env')
+
+FROM1 = os.environ['FROM1']
+TO1 = os.environ['TO1']
+password1 = os.environ['password1']
+
+# print(password1, TO1, FROM1)
+
+###### EMAIL   ########
+#####
+
+
+
+
+SUBJECT = 'Expense Tracker Data  '+ fileAttach +' email sent Successfully!'
+password = password1
+
+email = EmailMessage()
+email['Subject'] = SUBJECT
+email['From'] = FROM1
+email['To'] = TO1
+
+with open(path+ fileAttach, 'rb') as content_file:
+    content = content_file.read()
+    email.add_attachment(content, maintype='application', subtype='pdf', filename= fileAttach)
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.ehlo()
+s.starttls()
+s.login(FROM1, password)
+s.send_message(email)
+s.quit()
+print('Email sent for file with attachment: '+ fileAttach)
